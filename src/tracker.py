7c418f405a10a3094b1d5399786e899d59fd2587
Ritik <ritikchauhan132006@gmail.com>
2026-06-18 11:14:23 +0000

    restarted work

--- src/tracker.py
+++ src/tracker.py
@@ -8,10 +8,14 @@ import aiohttp
 from struct import unpack
 from urllib.parse import urlencode
 
+
 def generate_peer_ID() -> str:
-    return "-ZEPHYR-" + "".join(str(random.randint(0,9)) for i in range(12))
+    return "-ZEPHYR-" + "".join(str(random.randint(0, 9)) for i in range(12))
+
 
 # generation test
 # myid = generate_peer_ID()
 # print(myid)
 
+async def announce(torrent, peer_id: str, downloaded: int=0, uploaded: int=0, first: bool=false) -> dict:
+    
